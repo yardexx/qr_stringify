61d3a54e6d1d75f6ad4fb9d5afb2d3ce03106de2
Jaroslav Novotný <novotnyj912@gmail.com>
2023-01-08 22:20:43 +0100

    feat: Remove unused files

--- lib/src/qr_cli_drawer.dart
+++
@@ -1,7 +0,0 @@
-/// {@template qr_cli_drawer}
-/// A Very Good Project created by Very Good CLI.
-/// {@endtemplate}
-class QrCliDrawer {
-  /// {@macro qr_cli_drawer}
-  const QrCliDrawer();
-}
--- lib/src/qr_drawer.dart
+++
@@ -1,5 +0,0 @@
-import 'package:qr/qr.dart';
-
-abstract class QrDrawer {
-  String draw(QrCode code);
-}
