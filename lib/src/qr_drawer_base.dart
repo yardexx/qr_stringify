9721c24bc785e7113c5fa06a91284d6d6f75e8c8
Jaroslav Novotný <novotnyj912@gmail.com>
2023-01-10 19:12:21 +0100

    refactoring: Refactored drawing class

--- lib/src/qr_drawer_base.dart
+++ lib/src/qr_drawer_base.dart
@@ -1,8 +1,7 @@
 import 'package:qr_stringify/src/drawer.dart';
 import 'package:qr_stringify/src/enums.dart';
+import 'package:qr_stringify/src/extensions.dart';
 import 'package:qr_stringify/src/models/qr_code.dart';
-
-import './extensions.dart';
 
 abstract class QrDrawerBase extends Drawer {
   final _codeBuffer = StringBuffer();
@@ -25,33 +24,30 @@ abstract class QrDrawerBase extends Drawer {
 
   @override
   void addPadding(int padding) {
-    final lines = _codeBuffer.splitToLines();
-    final paddingBuffer = StringBuffer();
-
-    // calculate line
-    final paddingLine = (4 * padding + lines.first.length).space;
-
-    for (var i = 0; i < padding; i++) {
-      paddingBuffer.writeln(paddingLine);
-    }
-
-    lines.forEach((element) {
-      paddingBuffer.writeln(_createPaddedLine(element, padding));
-    });
-
-    for (var i = 0; i < padding; i++) {
-      paddingBuffer.writeln(paddingLine);
-    }
+    final lines = _codeBuffer.splitToLines()..removeLast();
 
     _codeBuffer
       ..clear()
-      ..write(paddingBuffer.toString());
+      ..write(_computeHorizontalPadding(padding, lines.first.length))
+      ..write('\n')
+      ..write(_computeVerticalPadding(padding, lines))
+      ..write(_computeHorizontalPadding(padding, lines.first.length));
   }
 
-  String _createPaddedLine(String content, int padding) {
+  String _createPaddedLine(int padding, String content) {
     return (2 * padding).space + content + (2 * padding).space;
   }
 
+  String _computeVerticalPadding(int padding, List<String> content) {
+    return content.map((line) => _createPaddedLine(padding, line)).join('\n');
+  }
+
+  String _computeHorizontalPadding(int padding, int lineLength) {
+    final paddingLine = (4 * padding + lineLength).space;
+    return List.generate(padding, (_) => paddingLine).join('\n');
+  }
+
+  @override
   String draw() {
     return _codeBuffer.toString();
   }
