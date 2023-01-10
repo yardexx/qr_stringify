cfd83637a634a019af45f790d972c2e994f60c32
Jaroslav Novotný <novotnyj912@gmail.com>
2023-01-10 19:13:00 +0100

    docs: Docs of extensions

--- lib/src/extensions.dart
+++ lib/src/extensions.dart
@@ -1,9 +1,19 @@
+/// An e3xtension to simplify work with [StringBuffer].
 extension StringBufferX on StringBuffer {
+  /// Breaks [String] into List<String>.
+  ///
+  /// String is split using newline separator.
+  /// Returns list of lines.
   List<String> splitToLines() {
     return toString().split('\n');
   }
 }
 
+/// An extension to simplify work with [int]
 extension IntX on int {
-  String get space => '  ' * this;
+  /// Represents [int] as unit of horizontal space.
+  ///
+  /// For each unit of [int], one space is added. Used as padding in terminal.
+  /// Returns spaced [String] which length is equal to [int].
+  String get space => ' ' * this;
 }
