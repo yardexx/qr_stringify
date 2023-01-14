717f5fbaf2da951c5cc1afffd3042f24acff9437
Jaroslav Novotný <novotnyj912@gmail.com>
2023-01-14 21:20:11 +0100

    feat(utf8): Refactoring + docs

--- lib/src/drawers/utf8.dart
+++ lib/src/drawers/utf8.dart
@@ -1,35 +1,50 @@
-import 'package:qr_stringify/src/models/qr_code.dart';
-import 'package:qr_stringify/src/qr_drawer_base.dart';
+import 'package:qr_stringify/qr_stringify.dart';
 
+/// A class which implements [QrDrawerBase].
+///
+/// Draws QR code using UTF-8 characters.
+///
+/// Checks for each *two* lines at once and figures out correct char.
 class Utf8 extends QrDrawerBase {
   static const _upperHalfBlock = '▀';
   static const _bottomHalfBlock = '▄';
   static const _fullBlock = '█';
   static const _space = ' ';
-  static const _newline = '\n';
 
   @override
   String drawCode(QrCode code) {
-    final qrImage = code.toMatrix();
+    final rawImage = code.toMatrix();
     final buffer = StringBuffer();
-    for (var x = 0; x < qrImage.length; x = x + 2) {
-      for (var y = 0; y < qrImage.length; y++) {
-        if (qrImage[x][y] == 1) {
-          if (x + 1 != qrImage.length && qrImage[x + 1][y] == 1) {
-            buffer.write(_fullBlock);
-          } else {
-            buffer.write(_upperHalfBlock);
-          }
-        } else {
-          if (x + 1 != qrImage.length && qrImage[x + 1][y] == 1) {
-            buffer.write(_bottomHalfBlock);
-          } else {
-            buffer.write(_space);
-          }
-        }
-      }
-      buffer.write(_newline);
+
+    for (var x = 0; x < rawImage.length; x = x + 2) {
+      buffer.writeln(_generateRow(rawImage, x).join());
     }
+
     return buffer.toString();
+  }
+
+  List<String> _generateRow(Matrix<int> rawData, int row) {
+    return List.generate(
+      rawData.length,
+      (column) => _findChar(rawData, row, column),
+    ).toList();
+  }
+
+  String _findChar(Matrix<int> rawData, int row, int column) {
+    return (rawData[row][column] == 1)
+        ? _checkFullBlock(rawData, row, column)
+        : _checkEmptyBlock(rawData, row, column);
+  }
+
+  String _checkEmptyBlock(Matrix<int> rawData, int row, int column) {
+    return (row + 1 != rawData.length && rawData[row + 1][column] == 1)
+        ? _bottomHalfBlock
+        : _space;
+  }
+
+  String _checkFullBlock(Matrix<int> rawImage, int row, int column) {
+    return (row + 1 != rawImage.length && rawImage[row + 1][column] == 1)
+        ? _fullBlock
+        : _upperHalfBlock;
   }
 }
